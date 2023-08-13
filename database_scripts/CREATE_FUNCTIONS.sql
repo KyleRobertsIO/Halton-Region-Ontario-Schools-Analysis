52c02161022b6ea6406548b24b721b1aa342ec02
KyleRobertsIO <kkroberts1635@gmail.com>
2023-08-13 02:44:49 +0000

    Added function Grade_Count for converting Grade_Range to a INT

--- database_scripts/CREATE_FUNCTIONS.sql
+++ database_scripts/CREATE_FUNCTIONS.sql
@@ -11,7 +11,39 @@ BEGIN
             WHEN 'N/D' THEN NULL
             WHEN 'N/R' THEN NULL
             ELSE CAST(REPLACE(@COL_VALUE, '%', '') AS DECIMAL(5, 2))
-            END
+        END
     )
     RETURN @RESULT_VALUE
 END;
+GO
+
+CREATE OR ALTER FUNCTION [dbo].[Grade_Count] (
+    @GRADE_RANGE NVARCHAR(4)
+)
+RETURNS INT AS
+BEGIN
+    DECLARE @TOTAL INT;
+    SET @TOTAL = (
+        SELECT
+        amount = CASE @GRADE_RANGE
+            WHEN '1-5' THEN 5
+            WHEN '1-8' THEN 8
+            WHEN '4-8' THEN 5
+            WHEN '5-8' THEN 4
+            WHEN '6-8' THEN 3
+            WHEN '7-8' THEN 2
+            WHEN '9-12' THEN 4
+            WHEN 'JK-2' THEN 4
+            WHEN 'JK-3' THEN 5
+            WHEN 'JK-4' THEN 6
+            WHEN 'JK-5' THEN 7
+            WHEN 'JK-6' THEN 8
+            WHEN 'JK-8' THEN 10
+            WHEN 'K-5' THEN 6
+            WHEN 'K-8' THEN 9
+            ELSE NULL
+        END
+    )
+    RETURN @TOTAL
+END;
+GO
